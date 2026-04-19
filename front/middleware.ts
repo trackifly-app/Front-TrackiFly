e2b5292486f77eb5f9fe82d1319018a22d690175
FlorCGutierrez <Florencia.carolina.gutierrez@gmail.com>
2026-04-19 16:49:16 -0300

    fix: middleware.ts

--- front/middleware.ts
+++ front/middleware.ts
@@ -1,12 +1,9 @@
 import createMiddleware from "next-intl/middleware";
-import { defineRouting } from "next-intl/routing";
 
-export default createMiddleware(
-  defineRouting({
-    locales: ["es", "en"],
-    defaultLocale: "es",
-  })
-);
+export default createMiddleware({
+  locales: ["es", "en"],
+  defaultLocale: "es",
+});
 
 export const config = {
   matcher: ["/((?!api|_next|.*\\..*).*)"],
