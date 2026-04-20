ebfdddd0484f8229d586a0ccbb1b9618fa53ef15
TomiMartinez112 <tomimartinez112@gmail.com>
2026-04-20 10:25:41 -0300

    Chatbot funcional

--- front/src/ui/home/HomePageView.tsx
+++ front/src/ui/home/HomePageView.tsx
@@ -1,21 +1,25 @@
-import TrackingSection from './sections/TrackingSection';
-import ShortcutSection from './sections/ShortcutsSection';
-import WhatToShipSection from './sections/WhatToShipSection';
-import LocationsSection from './sections/LocationsSection';
-import CallToActionSection from './sections/CallToActionSection';
-import CalcularEnvioPage from '../ordercalculatorView';
+import TrackingSection from "./sections/TrackingSection";
+import ShortcutSection from "./sections/ShortcutsSection";
+import WhatToShipSection from "./sections/WhatToShipSection";
+import LocationsSection from "./sections/LocationsSection";
+import CallToActionSection from "./sections/CallToActionSection";
+import CalcularEnvioPage from "../ordercalculatorView";
+import Chatbot from "@/components/Chatbot";
 
 const HomePageView = () => {
   return (
-    <div className="bg-background">
-      <div className="bg-surface px-50 py-16 grid grid-cols-1 lg:grid-cols-2 gap-8 items-start">
+    <div className="bg-white">
+      <div className="bg-gray-50 px-50 py-16 grid grid-cols-1">
         <TrackingSection />
         <CalcularEnvioPage />
       </div>
+
       <ShortcutSection />
       <WhatToShipSection />
       <LocationsSection />
       <CallToActionSection />
+
+      <Chatbot />
     </div>
   );
 };
