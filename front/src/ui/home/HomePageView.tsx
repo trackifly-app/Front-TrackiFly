514ca0e6a883fcbfd5970ca442efe25c2ffdc18c
AugustoMC97 <augustomc007@gmail.com>
2026-04-20 10:25:56 -0500

    Implementación del chat bot

--- front/src/ui/home/HomePageView.tsx
+++ front/src/ui/home/HomePageView.tsx
@@ -1,25 +1,29 @@
-import TrackingSection from "./sections/TrackingSection";
-import ShortcutSection from "./sections/ShortcutsSection";
-import WhatToShipSection from "./sections/WhatToShipSection";
-import LocationsSection from "./sections/LocationsSection";
-import CallToActionSection from "./sections/CallToActionSection";
-import CalcularEnvioPage from "../ordercalculatorView";
-import Chatbot from "@/components/Chatbot";
+import TrackingSection from './sections/TrackingSection';
+import ShortcutSection from './sections/ShortcutsSection';
+import WhatToShipSection from './sections/WhatToShipSection';
+import LocationsSection from './sections/LocationsSection';
+import CallToActionSection from './sections/CallToActionSection';
+import CalcularEnvioPage from '../ordercalculatorView';
 
 const HomePageView = () => {
   return (
-    <div className="bg-white">
-      <div className="bg-gray-50 px-50 py-16 grid grid-cols-1">
-        <TrackingSection />
-        <CalcularEnvioPage />
+    <div className="bg-background w-full overflow-x-hidden">
+      <div className="bg-surface w-full px-4 sm:px-6 md:px-10 lg:px-20 xl:px-32 py-10 md:py-14 lg:py-1 grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-1 items-start">
+        <div className="w-full">
+          <TrackingSection />
+        </div>
+
+        <div className="w-full">
+          <CalcularEnvioPage />
+        </div>
       </div>
 
-      <ShortcutSection />
-      <WhatToShipSection />
-      <LocationsSection />
-      <CallToActionSection />
-
-      <Chatbot />
+      <div className="space-y-12 md:space-y-16">
+        <ShortcutSection />
+        <WhatToShipSection />
+        <LocationsSection />
+        <CallToActionSection />
+      </div>
     </div>
   );
 };
